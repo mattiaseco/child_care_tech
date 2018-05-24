3a3da80359c55c401f4c4d0423745203c62b37c8
alberti-fra <alberti.francesco96@gmail.com>
2018-05-24 17:00:06 +0200

    AGGIUNTI metodi inserisci per EZIO

--- client/src/common/Interface/iGitaDAO.java
+++ client/src/common/Interface/iGitaDAO.java
@@ -1,6 +1,8 @@
 package common.Interface;
 
+import common.Classes.Bambino;
 import common.Classes.Gita;
+import common.Classes.Pullman;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
@@ -13,6 +15,6 @@ public interface iGitaDAO extends Remote {
     void inserisciGita(String destinazione, int num_pullman, int num_partecipanti, LocalDate data_partenza, LocalDate data_ritorno,String costo,String descrizione) throws RemoteException, SQLException;
     List<Gita> getAllGite() throws RemoteException,SQLException;
     void cancellaGita(int codice_g)throws RemoteException,SQLException;
-    void inserisciBambinoGita(int codice_g,String cf)throws RemoteException,SQLException;
-    void inserisciBambinoPullman(int codice_g,String cf)throws RemoteException,SQLException;
+    void inserisciBambinoGita(Gita gita, Bambino bambino)throws RemoteException,SQLException;
+    void inserisciBambinoPullman(Pullman pullman, Bambino bambino)throws RemoteException,SQLException;
 }
