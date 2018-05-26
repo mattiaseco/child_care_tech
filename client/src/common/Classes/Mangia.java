7fa04403bdaf5746bd2994e8cf464736d6e46430
alberti-fra <alberti.francesco96@gmail.com>
2018-05-26 15:12:40 +0200

    MANGIA ADDED

---
+++ client/src/common/Classes/Mangia.java
@@ -0,0 +1,29 @@
+package common.Classes;
+
+import java.io.Serializable;
+
+public class Mangia implements Serializable {
+
+
+    private Bambino cf;
+    private Menu numero;
+
+
+    public Mangia (Bambino cf,Menu numero){
+
+        this.cf=cf;
+        this.numero = numero;
+    }
+
+    public Bambino getCf() {
+        return cf;
+    }
+    public void setCf(Bambino cf) {
+        this.cf = cf;
+    }
+
+    public Menu getNumero(){ return  numero;}
+    public void setNumero(Menu numero) {
+        this.numero = numero;
+    }
+}
