bac6c9ea87681ad02497135400116ff9e1d5c50d
alberti-fra <alberti.francesco96@gmail.com>
2018-05-31 14:31:33 +0200

    PRESENZA CLASSES

---
+++ client/src/common/Classes/Presenza.java
@@ -0,0 +1,27 @@
+package common.Classes;
+
+import java.io.Serializable;
+
+public class Presenza implements Serializable {
+    private Bambino cf;
+
+
+    public Presenza(Bambino cf){
+        this.cf = cf;
+
+    }
+
+    public  Bambino getCf(){return cf;}
+
+    public void setCf(Bambino cf) {
+        this.cf = cf;
+    }
+
+    public boolean equals(Object obj) {
+        if(obj==this)
+            return true;
+        if(!(obj instanceof Bambino))
+            return false;
+        return this.cf.equals(((Bambino) obj).getCf());
+    }
+}
